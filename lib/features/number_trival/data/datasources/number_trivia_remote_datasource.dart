da4bc026f11b2f4607c393bcaa8126f5e0219de5
Priyaranjan <priyaranjanmantri@gmail.com>
2024-02-11 22:07:29 +0530

    ✏️ impl: remote data source

--- lib/features/number_trival/data/datasources/number_trivia_remote_datasource.dart
+++ lib/features/number_trival/data/datasources/number_trivia_remote_datasource.dart
@@ -1,3 +1,9 @@
+import 'dart:convert';
+
+import 'package:bloc_auth/core/apis/app_uri.dart';
+import 'package:bloc_auth/core/error/custom_exceptions.dart';
+import 'package:http/http.dart' as http;
+
 import '../models/number_trivia_model.dart';
 
 abstract class NumberTriviaRemoteDatasource {
@@ -10,4 +16,34 @@ abstract class NumberTriviaRemoteDatasource {
   ///
   /// throws a [ServerException] for all err codes
   Future<NumberTriviaModel> getRandomNumberTrivia();
+}
+
+class NumberTriviaRemoteDatasourceImpl implements NumberTriviaRemoteDatasource {
+  final http.Client httpClient;
+
+  NumberTriviaRemoteDatasourceImpl({
+    required this.httpClient,
+  });
+
+  @override
+  Future<NumberTriviaModel> getConcreteNumberTrivia(int number) async {
+    final response = await httpClient.get(AppUri.getUri(path: "$number?json"));
+
+    if (response.statusCode == 200) {
+      return NumberTriviaModel.fromJson(json.decode(response.body));
+    } else {
+      throw ServerException();
+    }
+  }
+
+  @override
+  Future<NumberTriviaModel> getRandomNumberTrivia() async {
+    final response = await httpClient.get(AppUri.getUri(path: "random?json"));
+
+    if (response.statusCode == 200) {
+      return NumberTriviaModel.fromJson(json.decode(response.body));
+    } else {
+      throw ServerException();
+    }
+  }
 }
