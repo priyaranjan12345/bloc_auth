04a4566231e69b1eb0fe6a865f4fd43a48d36272
Priyaranjan <priyaranjanmantri@gmail.com>
2024-04-28 00:13:10 +0530

    🔊 feat: impl get random and concrete number trivia

--- lib/features/number_trival/presentation/bloc/number_trivia_bloc.dart
+++ lib/features/number_trival/presentation/bloc/number_trivia_bloc.dart
@@ -1,6 +1,9 @@
+import 'package:bloc_auth/core/error/failuer.dart';
 import 'package:bloc_auth/core/utils/input_converter.dart';
+import 'package:bloc_auth/features/number_trival/domain/entities/number_tivia.dart';
 import 'package:bloc_auth/features/number_trival/domain/usecases/get_concrete_number_trivia.dart';
 import 'package:bloc_auth/features/number_trival/domain/usecases/get_random_number_trivia.dart';
+import 'package:dartz/dartz.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'bloc.dart';
@@ -15,8 +18,37 @@ class NumberTriviaBloc extends Bloc<NumberTriviaEvent, NumberTriviaState> {
     required this.getRandomNumberTrivia,
     required this.inputConverter,
   }) : super(const InitialNumberTriviaState()) {
-    on<GetTriviaForConcreteNumber>((event, emit) {});
+    /// Concrete Number Trivia
+    on<GetConcreteNumberTriviaEvent>((event, emit) {
+      final result = inputConverter.stringToUnsignedInteger(event.numberString);
+      result.fold((failure) {
+        emit(const ErrorNumberTriviaState(errMsg: 'Invalid input'));
+      }, (number) async {
+        final concreteNumberTrivia = await getConcreteNumberTrivia(
+          MyParams(number: number),
+        );
+        emitState(concreteNumberTrivia, emit);
+      });
+    });
 
-    on<GetTriviaForRandomNumber>((event, emit) {});
+    /// Random Number Trivia
+    on<GetRandomNumberTriviaEvent>((event, emit) async {
+      final randomNumberTrivia = await getRandomNumberTrivia(NoParams());
+      emitState(randomNumberTrivia, emit);
+    });
+  }
+
+  void emitState(
+    Either<Failure, NumberTrivia> numberTriviaResult,
+    Emitter<NumberTriviaState> emit,
+  ) {
+    numberTriviaResult.fold(
+      (failure) => emit(
+        const ErrorNumberTriviaState(errMsg: 'Failed to get data'),
+      ),
+      (numberTrivia) => emit(
+        LoadedNumberTriviaState(trivia: numberTrivia),
+      ),
+    );
   }
 }
