2e5cbfbd59120bf710b856d2d4039d975d409215
Priyaranjan <priyaranjanmantri@gmail.com>
2024-04-28 00:11:56 +0530

    ✨ feat: number trivia UI

---
+++ lib/features/number_trival/presentation/view/number_trivia_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import 'number_trivia_view.dart';
+
+class NumberTriviaPage extends StatelessWidget {
+  const NumberTriviaPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const NumberTriviaView();
+  }
+}
