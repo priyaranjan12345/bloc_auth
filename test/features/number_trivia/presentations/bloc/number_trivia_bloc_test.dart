b98069b0890114394d14d8cad06691616c8b0642
Priyaranjan <priyaranjanmantri@gmail.com>
2024-05-01 21:38:44 +0530

    🧪 test: unit test

--- test/features/number_trivia/presentations/bloc/number_trivia_bloc_test.dart
+++ test/features/number_trivia/presentations/bloc/number_trivia_bloc_test.dart
@@ -1,3 +1,4 @@
+import 'package:bloc_auth/core/error/failuer.dart';
 import 'package:bloc_auth/features/number_trival/domain/entities/number_tivia.dart';
 import 'package:bloc_test/bloc_test.dart';
 import 'package:dartz/dartz.dart';
@@ -46,6 +47,7 @@ void main() {
   });
 
   group("GetTriviaForConcreteNumber", () {
+    const tNumberTrivia = NumberTrivia(text: 'test number ext', number: 2);
     test(
         "should call the input converter to validate and convert the string to an unsigned",
         () async {
@@ -54,6 +56,12 @@ void main() {
         () => inputConverter.stringToUnsignedInteger(any()),
       ).thenReturn(
         const Right(tNumberParsed),
+      );
+
+      when(
+        () => getConcreteNumberTrivia(const Params(number: tNumberParsed)),
+      ).thenAnswer(
+        (invocation) async => const Right(tNumberTrivia),
       );
 
       // act
@@ -88,9 +96,144 @@ void main() {
         const ErrorNumberTriviaState(errMsg: 'Invalid input'),
       );
     });
+
+    test("should emit [loading, loaded] state when data is gotten successfully",
+        () async {
+      // arrange
+      when(
+        () => inputConverter.stringToUnsignedInteger(any()),
+      ).thenReturn(
+        const Right(tNumberParsed),
+      );
+
+      when(
+        () => getConcreteNumberTrivia(const Params(number: tNumberParsed)),
+      ).thenAnswer(
+        (invocation) async => const Right(tNumberTrivia),
+      );
+
+      final expected = [
+        const LoadingNumberTriviaState(),
+        const LoadedNumberTriviaState(trivia: tNumberTrivia),
+      ];
+
+      // assert
+      expectLater(numberTriviaBloc.stream, emitsInOrder(expected));
+
+      // act
+      numberTriviaBloc.add(
+        GetConcreteNumberTriviaEvent(numberString: tNumberString),
+      );
+    });
+
+    test(
+      'should emit [loading, error] state when data is gotten failure',
+      () {
+        // arrange
+        when(
+          () => inputConverter.stringToUnsignedInteger(any()),
+        ).thenReturn(Left(InvalidInputFailure()));
+
+        when(
+          () => getConcreteNumberTrivia(const Params(number: tNumberParsed)),
+        ).thenAnswer(
+          (invocation) async => const Right(tNumberTrivia),
+        );
+
+        final expected = [
+          const LoadingNumberTriviaState(),
+          const ErrorNumberTriviaState(errMsg: 'Invalid input'),
+        ];
+
+        // assert
+        expectLater(numberTriviaBloc.stream, emitsInOrder(expected));
+
+        // act
+        numberTriviaBloc.add(
+          GetConcreteNumberTriviaEvent(numberString: tNumberString),
+        );
+      },
+    );
+
+    test(
+        'should emit [loading, serverError] state, on server error but valid input number string',
+        () {
+      // arrange
+      when(
+        () => inputConverter.stringToUnsignedInteger(any()),
+      ).thenReturn(
+        const Right(tNumberParsed),
+      );
+
+      when(
+        () => getConcreteNumberTrivia(const Params(number: tNumberParsed)),
+      ).thenAnswer(
+        (invocation) async => Left(ServerFailure()),
+      );
+
+      final expected = [
+        const LoadingNumberTriviaState(),
+        const ErrorNumberTriviaState(errMsg: 'Server error'),
+      ];
+
+      // assert
+      expectLater(numberTriviaBloc.stream, emitsInOrder(expected));
+
+      // act
+      numberTriviaBloc.add(
+        GetConcreteNumberTriviaEvent(numberString: tNumberString),
+      );
+    });
   });
 
-  group("test number trivia bloc - with bloc test - ", () {
+  group('test GetRandomNumberTrivia', () {
+    const tNumberTrivia = NumberTrivia(
+      text: 'test random number trivia',
+      number: 1,
+    );
+    test('should emits [loading, loaded] stete on server success', () {
+      // arrange
+      when(
+        () => getRandomNumberTrivia(NoParams()),
+      ).thenAnswer(
+        (invocation) async => const Right(tNumberTrivia),
+      );
+
+      // assert
+      final expected = [
+        const LoadingNumberTriviaState(),
+        const LoadedNumberTriviaState(trivia: tNumberTrivia),
+      ];
+
+      expectLater(numberTriviaBloc.stream, emitsInOrder(expected));
+
+      // act
+      numberTriviaBloc.add(GetRandomNumberTriviaEvent());
+    });
+    test('should emits [loading, error] stete on server success', () {
+      // arrange
+      when(
+        () => getRandomNumberTrivia(NoParams()),
+      ).thenAnswer(
+        (invocation) async => Left(ServerFailure()),
+      );
+
+      // assert
+      final expected = [
+        const LoadingNumberTriviaState(),
+        const ErrorNumberTriviaState(errMsg: 'Server error'),
+      ];
+
+      expectLater(numberTriviaBloc.stream, emitsInOrder(expected));
+
+      // act
+      numberTriviaBloc.add(GetRandomNumberTriviaEvent());
+    });
+  });
+
+  group(
+      "test number trivia bloc for concrete number trivia - with bloc test - ",
+      () {
     const tNumberTrivia = NumberTrivia(text: 'test number', number: 1);
     // const tNumberTriviaExt = NumberTrivia(text: 'test number ext', number: 2);
 
@@ -122,6 +265,7 @@ void main() {
         GetConcreteNumberTriviaEvent(numberString: tNumberString),
       ),
       expect: () => <NumberTriviaState>[
+        const LoadingNumberTriviaState(),
         const ErrorNumberTriviaState(errMsg: 'Invalid input'),
       ],
     );
@@ -136,7 +280,7 @@ void main() {
         );
 
         when(
-          () => getConcreteNumberTrivia(const MyParams(number: tNumberParsed)),
+          () => getConcreteNumberTrivia(const Params(number: tNumberParsed)),
         ).thenAnswer((invocation) async => const Right(tNumberTrivia));
       },
       build: () => NumberTriviaBloc(
@@ -148,7 +292,62 @@ void main() {
         GetConcreteNumberTriviaEvent(numberString: tNumberString),
       ),
       expect: () => <NumberTriviaState>[
+        const LoadingNumberTriviaState(),
         const LoadedNumberTriviaState(trivia: tNumberTrivia),
+      ],
+      verify: (bloc) => getConcreteNumberTrivia(
+        const Params(number: tNumberParsed),
+      ),
+    );
+  });
+
+  group('test numberTriviaBloc for randomNumberTrivia - with blic test', () {
+    const tNumberTrivia = NumberTrivia(text: 'test number ext', number: 2);
+    blocTest(
+      "should emits [intial] state",
+      build: () => NumberTriviaBloc(
+        getConcreteNumberTrivia: getConcreteNumberTrivia,
+        getRandomNumberTrivia: getRandomNumberTrivia,
+        inputConverter: inputConverter,
+      ),
+      expect: () => <NumberTriviaState>[],
+    );
+
+    blocTest(
+      "should emits [loading, loaded] state",
+      setUp: () => when(
+        () => getRandomNumberTrivia(NoParams()),
+      ).thenAnswer((invocation) async => const Right(tNumberTrivia)),
+      build: () => NumberTriviaBloc(
+        getConcreteNumberTrivia: getConcreteNumberTrivia,
+        getRandomNumberTrivia: getRandomNumberTrivia,
+        inputConverter: inputConverter,
+      ),
+      act: (bloc) => bloc.add(
+        GetRandomNumberTriviaEvent(),
+      ),
+      expect: () => <NumberTriviaState>[
+        const LoadingNumberTriviaState(),
+        const LoadedNumberTriviaState(trivia: tNumberTrivia),
+      ],
+    );
+
+    blocTest(
+      "should emits [loading, error] state",
+      setUp: () => when(
+        () => getRandomNumberTrivia(NoParams()),
+      ).thenAnswer((invocation) async =>Left(ServerFailure())),
+      build: () => NumberTriviaBloc(
+        getConcreteNumberTrivia: getConcreteNumberTrivia,
+        getRandomNumberTrivia: getRandomNumberTrivia,
+        inputConverter: inputConverter,
+      ),
+      act: (bloc) => bloc.add(
+        GetRandomNumberTriviaEvent(),
+      ),
+      expect: () => <NumberTriviaState>[
+        const LoadingNumberTriviaState(),
+        const ErrorNumberTriviaState(errMsg: 'Server error'),
       ],
     );
   });
